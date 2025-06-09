2b5ced3179d153f78ac091830729889dce5b3343
emma-mendez <emmamendez07@gmail.com>
2025-06-09 13:11:23 +0100

    deleting main

--- Projects/KLPS/info.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Generate a nonce value (random bytes, base64 encoded)
-$nonce = base64_encode(random_bytes(16));
-
-// Set the Content-Security-Policy header with the generated nonce
-header("Content-Security-Policy: default-src 'self'; script-src 'self' 'nonce-{$nonce}'; style-src 'self' 'nonce-{$nonce}';");
-
-?>
