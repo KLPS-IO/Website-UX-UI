06051983dedf53764de014e921691d2667710187
Emma Mendez <emmamendez07@gmail.com>
2026-04-10 16:31:33 +0100

    adding first 5 new users

--- src/config/betaAccounts.ts
+++ src/config/betaAccounts.ts
@@ -35,4 +35,9 @@ export const BETA_ACCOUNTS = [
     email: "Lisha586@gmail.com",
     password: "Lema04", // Phylisha April cohort test
   },
+  {
+    id: "88888888-8888-8888-8888-888888888888",
+    email: "Chanel-monet1@outlook.com",
+    password: "Lema04", // Chanel April cohort test
+  }
 ];
