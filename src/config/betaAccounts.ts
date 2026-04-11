44c1acb4061b3a80f7c4e8355b9039cd2ad3e2ea
Emma Mendez <emmamendez07@gmail.com>
2026-04-11 10:36:08 +0100

    adding new user

--- src/config/betaAccounts.ts
+++ src/config/betaAccounts.ts
@@ -4,45 +4,50 @@ export const BETA_ACCOUNTS = [
     email: "emmamendez07@gmail.com",
     password: "SheBuilds1.0", // (EMMA)
   },
-  // April cohort test
+  // April cohort
   {
     id: "22222222-2222-2222-2222-222222222222",
     email: "klpsglobal@gmail.com",
-    password: "Lema04", // Emma April cohort test
+    password: "Lema04", // Emma April cohort
   },
   {
     id: "33333333-3333-3333-3333-333333333333",
     email: "msamuel@live.co.uk",
-    password: "Lema04", // Mom April cohort test
+    password: "Lema04", // Mom April cohort
   },
   {
     id: "44444444-4444-4444-4444-444444444444",
     email: "Theycallmeebsie@icloud.com",
-    password: "Lema04", // Ebony April cohort test    
+    password: "Lema04", // Ebony April cohort    
   },
   {
     id: "55555555-5555-5555-5555-555555555555",
     email: "Varlene@hotmail.co.uk",
-    password: "Lema04", // Varlene April cohort test
+    password: "Lema04", // Varlene April cohort
   },
   {
     id: "66666666-6666-6666-6666-666666666666",
     email: "Akamgangguemgne@gmail.com",
-    password: "Lema04", // Michelle April cohort test  
+    password: "Lema04", // Michelle April cohort  
   },
   {
     id: "77777777-7777-7777-7777-777777777777",
     email: "Lisha586@gmail.com",
-    password: "Lema04", // Phylisha April cohort test
+    password: "Lema04", // Phylisha April cohort
   },
   {
     id: "88888888-8888-8888-8888-888888888888",
     email: "Chanel-monet1@outlook.com",
-    password: "Lema04", // Chanel April cohort test
+    password: "Lema04", // Chanel April cohort
   },
   {
     id: "99999999-9999-9999-9999-999999999999",
     email: "Ch3rrybom@mail.com",
-    password: "Lema04", // Test User
+    password: "Lema04", // Cherry April cohort
+  },
+  {
+    id: "10101010-1010-1010-1010-101010101010",
+    email: "Mbclarke1.mc@gmail.com",
+    password: "Lema04", // Monique April cohort
   }
 ];
