baa1583ab771f1a61ff26d56935ad02fb5fda1c2
Emma Mendez <emmamendez07@gmail.com>
2026-07-10 11:26:10 +0100

    small change pitch deck

--- src/components/data/slides.tsx
+++ src/components/data/slides.tsx
@@ -202,7 +202,7 @@ function Slide01() {
         <div
           style={{
             position: "absolute",
-            top: 250,
+            top: 450,
             left: 780,
           }}
         >
